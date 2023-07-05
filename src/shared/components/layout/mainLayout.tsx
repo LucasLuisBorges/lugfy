7d54a0c78f0d5278b2467a4326259b8a93e70a3d
Lucas Luis Borges <lucasluisborges@outlook.com>
2023-07-05 19:32:54 -0300

    feat: responsive

--- src/shared/components/layout/mainLayout.tsx
+++ src/shared/components/layout/mainLayout.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { ReactNode, useState } from 'react';
-import Image from 'next/image';
 import { House, MagnifyingGlass, Book, List } from 'phosphor-react';
 import SplitPane, { Pane } from 'split-pane-react';
 import 'split-pane-react/esm/themes/default.css';
 import { useMediaQuery } from 'react-responsive';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
+import { UserButton, useAuth } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
 
 import {
   LanguageButton,
@@ -17,8 +18,7 @@ import {
   PlayerView,
   MobilePlayerView,
 } from '../index';
-import { useRouter } from 'next/navigation';
-import { UserButton, useAuth } from '@clerk/nextjs';
+import { Icons } from '../icons/icons';
 
 interface IProps {
   children: ReactNode;
@@ -27,11 +27,17 @@ interface IProps {
 const MainLayoutComponent = ({ children }: IProps) => {
   const router = useRouter();
   const { userId } = useAuth();
-  const [sizes, setSizes] = useState([25, 75]);
+  const isBigScreenForMenu = useMediaQuery({ minWidth: 1600 });
+  const [sizes, setSizes] = useState([isBigScreenForMenu ? 15 : 25, 100]);
   const [languageModal, setLanguageModal] = useState(false);
+  const isBigScreen = useMediaQuery({ minWidth: 1024 });
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function HandleModal() {
+    setMenuOpen(!menuOpen);
+  }
 
   const sashRender = () => <div className="cursor-col-resize" />;
-  const isBigScreen = useMediaQuery({ minWidth: 1024 });
 
   const toggleModal = () => {
     setLanguageModal(!languageModal);
@@ -44,16 +50,11 @@ const MainLayoutComponent = ({ children }: IProps) => {
         onChange={setSizes}
         sashRender={sashRender}
       >
-        <Pane minSize={200} maxSize="25%" className="flex">
+        <Pane minSize={230} maxSize="25%" className="flex">
           <section className="sticky flex flex-col w-full h-[92vh] max-h-[92vh] bg-[#030303] text-white">
-            <Link href="/">
-              <Image
-                src="/logo.png"
-                className="m-6 cursor-pointer"
-                alt="Logo"
-                width={131}
-                height={40}
-              />
+            <Link href="/" className="flex items-center">
+              <Icons.logo className="fill-white h-14 w-14 m-6" />
+              <span className="uppercase text-2xl font-light">Lugfy</span>
             </Link>
             <nav className="flex flex-col h-full justify-between items-center">
               <div className="flex w-full flex-col gap-1">
@@ -85,15 +86,6 @@ const MainLayoutComponent = ({ children }: IProps) => {
         <Pane>
           <section className="relative flex-1 bg-gradient-to-br from-[#1f1f1f] to-[#1f1e1e]">
             <div className="sticky flex top-0 left-0 w-full px-8 bg-[#121212] h-16 items-center justify-between z-50">
-              <Link href="/" className="flex lg:hidden">
-                <Image
-                  src="/logo.png"
-                  className="m-6 cursor-pointer"
-                  alt="Logo"
-                  width={131}
-                  height={40}
-                />
-              </Link>
               <div className="flex gap-2">
                 <ArrowButton variant="left" onClick={() => router.back()} />
                 <ArrowButton variant="right" onClick={() => router.forward()} />
@@ -131,14 +123,11 @@ const MainLayoutComponent = ({ children }: IProps) => {
   ) : (
     <section className="relative flex-1 min-h-screen bg-gradient-to-tl from-[#1f1f1f] to-[#2e2e2e]">
       <div className="sticky z-[100] flex top-0 left-0 w-full px-8 bg-[#121212] h-16 items-center justify-between">
-        <Link href="/" className="flex">
-          <Image
-            src="/logo.png"
-            className="m-6 cursor-pointer"
-            alt="Logo"
-            width={131}
-            height={40}
-          />
+        <Link href="/" className="flex items-center">
+          <Icons.logo className="fill-white h-10 w-10 m-6" />
+          <span className="uppercase text-2xl font-light text-white">
+            Lugfy
+          </span>
         </Link>
         <div className="flex gap-6">
           <MagnifyingGlass
@@ -147,11 +136,24 @@ const MainLayoutComponent = ({ children }: IProps) => {
           />
           <List
             size={28}
-            className="text-white hover:scale-105 transition duration-300"
+            className="text-white hover:scale-105 transition duration-300 hover:cursor-pointer"
+            onClick={HandleModal}
           />
         </div>
       </div>
       <section className="py-5 pl-9">{children}</section>
+      {!isBigScreen && menuOpen && (
+        <div className="absolute z-[100] top-0 left-0 w-screen h-screen bg-zinc-950/95">
+          <div className="w-full flex justify-end">
+            <span
+              onClick={HandleModal}
+              className="text-white mt-4 mr-8 text-3xl hover:cursor-pointer"
+            >
+              X
+            </span>
+          </div>
+        </div>
+      )}
       <MobilePlayerView />
     </section>
   );
